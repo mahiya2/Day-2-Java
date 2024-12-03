c2e2415ada6a932adff35bd213c770379bf30c58
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-03 10:21:38 +0530

    Add files via upload

---
+++ Arith.java
@@ -0,0 +1,11 @@
+class Arith{
+    public static void main(String args[]){
+        int a=45;
+        int b=5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
