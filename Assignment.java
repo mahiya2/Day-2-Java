2db51aebe410b8159fca1fd3abe49b4a2108a7cd
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-03 10:59:21 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Assignment{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    System.out.println(a+=46);
+    System.out.println(a-=9);
+    System.out.println(a*=2);
+}
+}
