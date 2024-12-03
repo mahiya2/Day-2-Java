2d99a34f92b130681e0ac3bbb39474e69b746411
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-03 15:10:09 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,12 @@
+public class Logical{
+    public static void main(String args[]){
+        System.out.println(true&&true);
+        System.out.println(false||true);
+        System.out.println(true&&false);
+        System.out.println(false&&false);
+        System.out.println(false||false);
+        System.out.println(true||false);
+        System.out.println(!(true));
+        System.out.println(!(false));
+    }
+}
